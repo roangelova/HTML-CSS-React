d8abc9bbc8b350c8203a9c607bc82790c6b8d1db
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-12 18:44:27 +0300

    conditional rendering added

--- Workshop- Components/GamesPlay/gamesplay/src/components/GameCatalog/GameCatalog.js
+++ Workshop- Components/GamesPlay/gamesplay/src/components/GameCatalog/GameCatalog.js
@@ -5,6 +5,7 @@ const GameCatalog = () => {
     const [games, setGames] = useState([]);
 
     useEffect(() => {
+
         fetch('https://localhost:3030/data/games?sortBy=_createdOn%20desc')
             .then(res => res.json())
             .then(res => {
@@ -16,9 +17,12 @@ const GameCatalog = () => {
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.map(x => <GameCard game={x} />)}
+            {
+                games.length > 0
+                    ? games.map(x => <GameCard key={x._id} game={x} />)
+                    : <h3 class="no-articles">No articles yet</h3>
+            }
 
-            <h3 class="no-articles">No articles yet</h3>
         </section>
     );
 };
