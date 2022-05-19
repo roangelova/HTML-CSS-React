3217644c8fec4ddd13c7c5d7ebb797b5fbd02bf4
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-19 16:26:36 +0300

    added getOne method

--- Workshop- Components/GamesPlay/gamesplay/src/services/GameService.js
+++ Workshop- Components/GamesPlay/gamesplay/src/services/GameService.js
@@ -4,3 +4,6 @@ export function getAll() {
     return fetch(`${baseUrl}/games?sortBy=_createdOn%20desc`)
         .then(res => res.json())
 }
+
+export const getOne = (id) =>
+    fetch(`${baseUrl}/games/${id}`).then(res => res.json());
