9790b08b63883c1e429b0fdbaa403b1577debbb4
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-23 17:20:37 +0300

    Links added

--- React/Workshop - SPA/PetMyPet/petmypet/src/components/Header/Header.js
+++ React/Workshop - SPA/PetMyPet/petmypet/src/components/Header/Header.js
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (<header id="site-header">
         <nav className="navbar">
             <section className="navbar-dashboard">
-                <a href="#">Dashboard</a>
+                <Link to="/">Dashboard</Link>
                 <div id="guest">
-                    <a className="button" href="#">Login</a>
-                    <a className="button" href="#">Register</a>
+                    <Link className="button" to="/login">Login</Link>
+                    <Link className="button" to="/register">Register</Link>
                 </div>
                 <div id="user">
                     <span>Welcome, Roslava</span>
-                    <a className="button" href="#">My Pets</a>
-                    <a className="button" href="#">Add Pet</a>
-                    <a className="button" href="#">Logout</a>
+                    <Link className="button" to="/my-pets">My Pets</Link>
+                    <Link className="button" to="/create">Add Pet</Link>
+                    <Link className="button" to="#">Logout</Link>
                 </div>
             </section>
         </nav>
