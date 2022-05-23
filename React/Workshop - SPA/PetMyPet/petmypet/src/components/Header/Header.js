fd84a8fad712e54f56776d2707cc01b56c4f0430
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-23 16:50:44 +0300

    components created

---
+++ React/Workshop - SPA/PetMyPet/petmypet/src/components/Header/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (<header id="site-header">
+        <nav className="navbar">
+            <section className="navbar-dashboard">
+                <a href="#">Dashboard</a>
+                <div id="guest">
+                    <a className="button" href="#">Login</a>
+                    <a className="button" href="#">Register</a>
+                </div>
+                <div id="user">
+                    <span>Welcome, Roslava</span>
+                    <a className="button" href="#">My Pets</a>
+                    <a className="button" href="#">Add Pet</a>
+                    <a className="button" href="#">Logout</a>
+                </div>
+            </section>
+        </nav>
+    </header>)
+}
+
+export default Header;
